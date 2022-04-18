a5df666858eca3d2963d9aeef8d19918f17cce77
Ethan D. Twardy <ethan.twardy@gmail.com>
2022-04-18 18:43:12 -0500

    Implement basic proxying and file serving

--- src/main.rs
+++ src/main.rs
@@ -12,13 +12,15 @@
 
 use core::convert::Infallible;
 use core::task::{Context, Poll};
-use core::future::{self, Future};
+use core::future::Future;
 use core::pin::Pin;
 
 use std::env::current_dir;
+use std::error::Error;
 use std::fs::File;
 use std::io::{self, Read};
 use std::path::PathBuf;
+use std::fmt;
 
 use hyper::{
     Body, Client,
@@ -28,6 +30,69 @@ use hyper::{
     service::{make_service_fn, Service},
     Uri,
 };
+
+///////////////////////////////////////////////////////////////////////////////
+// ProxyError
+////
+
+#[derive(Debug)]
+pub enum ProxyError {
+    Proxy(io::Error),
+    Http(hyper::Error),
+}
+
+impl fmt::Display for ProxyError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match &self {
+            Self::Proxy(error) => write!(f, "{}", error),
+            Self::Http(error) => write!(f, "{}", error),
+        }
+    }
+}
+
+impl From<io::Error> for ProxyError {
+    fn from(error: io::Error) -> Self {
+        Self::Proxy(error)
+    }
+}
+
+impl From<hyper::Error> for ProxyError {
+    fn from(error: hyper::Error) -> Self {
+        Self::Http(error)
+    }
+}
+
+impl Error for ProxyError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        None
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// ProxyResponseFuture
+////
+
+struct ProxyResponseFuture(ResponseFuture);
+impl Future for ProxyResponseFuture {
+    type Output = Result<Response<Body>, ProxyError>;
+    fn poll(mut self: Pin<&mut Self>, context: &mut Context<'_>) ->
+        Poll<Self::Output>
+    {
+        match Pin::new(&mut self.0).poll(context) {
+            Poll::Pending => Poll::Pending,
+            Poll::Ready(response) => match response {
+                Ok(response) => Poll::Ready(Ok(response)),
+                Err(err) => Poll::Ready(Err(err.into())),
+            },
+        }
+    }
+}
+
+impl From<ResponseFuture> for ProxyResponseFuture {
+    fn from(response: ResponseFuture) -> Self {
+        Self(response)
+    }
+}
 
 ///////////////////////////////////////////////////////////////////////////////
 // Proxy
@@ -45,13 +110,13 @@ impl ProxyRoute {
         Self { route, proxy, client: Client::new() }
     }
 
-    pub fn matched(&self, path: &str) -> bool {
+    pub fn matches(&self, path: &str) -> bool {
         path.starts_with(&self.route)
     }
 
-    pub fn proxy(&self, request: Request<Body>) -> ResponseFuture {
+    pub fn request(&self, request: Request<Body>) -> ProxyResponseFuture {
         let uri: Uri = (
-            self.route.to_string()
+            self.proxy.to_string()
                 + request.uri().path().strip_prefix(&self.route).unwrap())
             .parse().unwrap();
         let proxy_request = Request::builder()
@@ -59,7 +124,7 @@ impl ProxyRoute {
             .uri(uri)
             .body(request.into_body())
             .unwrap();
-        self.client.request(proxy_request)
+        self.client.request(proxy_request).into()
     }
 }
 
@@ -71,8 +136,14 @@ struct StaticFileFuture {
     path: PathBuf,
 }
 
+impl StaticFileFuture {
+    pub fn new(path: PathBuf) -> Self {
+        Self { path }
+    }
+}
+
 impl Future for StaticFileFuture {
-    type Output = io::Result<Response<Body>>;
+    type Output = Result<Response<Body>, ProxyError>;
     fn poll(self: Pin<&mut Self>, _context: &mut Context<'_>) ->
         Poll<Self::Output>
     {
@@ -85,7 +156,7 @@ impl Future for StaticFileFuture {
                 match file.read_to_string(&mut contents) {
                     Ok(_) => Ok(Response::builder().status(200)
                                 .body(Body::from(contents)).unwrap()),
-                    Err(error) => Err(error),
+                    Err(error) => Err(error.into()),
                 }
             },
 
@@ -95,7 +166,7 @@ impl Future for StaticFileFuture {
                         Response::builder().status(404)
                             .body(Body::empty()).unwrap()
                     ),
-                    _ => Err(error),
+                    _ => Err(error.into()),
                 }
             },
         };
@@ -126,7 +197,7 @@ impl DevProxService {
 
 impl Service<Request<Body>> for DevProxService {
     type Response = Response<Body>;
-    type Error = Infallible;
+    type Error = ProxyError;
     type Future = Pin<Box<dyn Future<
             Output = Result<Self::Response, Self::Error>> + Send + Sync>>;
 
@@ -134,13 +205,14 @@ impl Service<Request<Body>> for DevProxService {
         Poll<Result<(), Self::Error>>
     { Ok(()).into() }
 
-    fn call(&mut self, _request: Request<Body>) -> Self::Future {
-        Box::pin(future::ready(Ok(
-            Response::builder()
-                .status(200)
-                .body(Body::from("Hello, world!"))
-                .unwrap()
-        )))
+    fn call(&mut self, request: Request<Body>) -> Self::Future {
+        let path = request.uri().path();
+        if let Some(proxy) = self.proxies.iter().find(|p| p.matches(path)) {
+            return Box::pin(proxy.request(request));
+        }
+
+        Box::pin(StaticFileFuture::new(
+            self.root.join(path.strip_prefix("/").unwrap())))
     }
 }
 
@@ -150,8 +222,12 @@ impl Service<Request<Body>> for DevProxService {
 
 #[tokio::main]
 async fn main() {
-    let service = DevProxService::new(current_dir().unwrap());
-    hyper::Server::bind(&"127.0.0.1:3000".parse().unwrap())
+    let mut service = DevProxService::new(current_dir().unwrap());
+    service.proxy(ProxyRoute::new(
+        "/api".to_string(),
+        "http://localhost:3000/api".parse().unwrap()
+    ));
+    hyper::Server::bind(&"127.0.0.1:8080".parse().unwrap())
         .serve(make_service_fn(|_: &AddrStream| {
             let service = service.clone();
             async move { Ok::<_, Infallible>(service) }
